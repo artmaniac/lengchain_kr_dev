8708b4ad9e6392434420fb69aafffda8c5ecb27b
artmaniac <32432700+artmaniac@users.noreply.github.com>
2024-04-10 01:26:28 +0900

    4

--- utils.py
+++ utils.py
@@ -12,9 +12,8 @@ class StreanHandler(BaseCallbackHandler):
 
 
 
-
 def print_messages(): 
     # 이전 대화기록을 출력해 주는 코드 
     if "messages" in st.session_state and len(st.session_state["messages"]) > 0:
-        for role, message in st.session_state["messages"]: 
+        for chat_message in st.session_state["messages"]: 
             st.chat_message(chat_message.role).write(chat_message.content)
